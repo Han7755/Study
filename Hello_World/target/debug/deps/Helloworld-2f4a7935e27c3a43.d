100ac53c5eba27520777c59092537a7eafee4fa4
Han7755 <gksmfgyrud@naver.com>
2022-11-29 15:36:20 +0900

    Hello World!

---
+++ Hello_World/target/debug/deps/Helloworld-2f4a7935e27c3a43.d
@@ -0,0 +1,5 @@
+c:\Users\윤하늘\OneDrive - DongEui\VS Code\Rust\Helloworld\target\debug\deps\Helloworld-2f4a7935e27c3a43.rmeta: src\main.rs
+
+c:\Users\윤하늘\OneDrive - DongEui\VS Code\Rust\Helloworld\target\debug\deps\Helloworld-2f4a7935e27c3a43.d: src\main.rs
+
+src\main.rs:
---
+++ Hello_World/target/debug/deps/Helloworld-5ea2aad416310394.d
@@ -0,0 +1,5 @@
+c:\Users\윤하늘\OneDrive - DongEui\VS Code\Rust\Helloworld\target\debug\deps\Helloworld-5ea2aad416310394.rmeta: src\main.rs
+
+c:\Users\윤하늘\OneDrive - DongEui\VS Code\Rust\Helloworld\target\debug\deps\Helloworld-5ea2aad416310394.d: src\main.rs
+
+src\main.rs:
