1342616ad4bfbc04ebe3cebaebbab25a4057fdc3
Han7755 <gksmfgyrud@naver.com>
2022-12-02 10:32:14 +0900

    let mut 으로 수정가능한 변수 생성하기

--- Modify_Varialbles/src/main.rs
+++ Modify_Varialbles/src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    let x: i32 = 32; // x 는 Int32형식의 변수 (수정 불가이지만 Constant는 아님)
+    println!("{}", x); // print value of x
+                       // 일반적으로 변수는 변경할 수 없음
+    let x: i32 = 5; // let 으로 변수를 새로 생성하여 같은 이름이지만 다른 값의 변수를 생성할 수 있음
+    println!("{}", x);
+
+    println!();
+    let mut y: i32 = 2998; // let mut 으로 수정할 수 있는 변수를 선언할 수 있음
+    println!("{}", y);
+
+    y = 3200;
+    println!("{}", y);
 }
