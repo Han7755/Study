732aa750b9057c06702801474f0971e739ed94c5
Han7755 <gksmfgyrud@naver.com>
2022-12-02 11:35:53 +0900

    Add Print

--- using_function/src/main.rs
+++ using_function/src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
     let y = {
         let x = 3;
-        x+1         // rust에서는 ;이 없으면 return 한다는 의미로 사용됨.
-    }
+        x + 1 // rust에서는 ;이 없으면 return 한다는 의미로 사용됨.
+    };
+    println!("{}", y);
 }
