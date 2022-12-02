cc6212bfa291e258e08a1d5d877ed16d94c5218a
Han7755 <gksmfgyrud@naver.com>
2022-12-02 11:46:38 +0900

    else if문

--- control_flow/src/main.rs
+++ control_flow/src/main.rs
@@ -1,8 +1,10 @@
 fn main() {
-    // if 문
+    // if 문 <- C++과 비슷하나, 숫자형을 조건에 사용할 수 없음
     let number = 3;
     if number < 5 {
         println!("number is smaller than 5");
+    } else if number == 5 {
+        println!("number is 5");
     } else {
         println!("number is greater than 5");
     }
