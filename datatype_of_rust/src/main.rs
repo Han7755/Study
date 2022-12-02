abc08cb5e08a0ae523c10610c8e009ec9423d620
Han7755 <gksmfgyrud@naver.com>
2022-12-02 10:48:29 +0900

    정수타입 변수 선언

--- datatype_of_rust/src/main.rs
+++ datatype_of_rust/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    // 아래의 변수 타입에서 i를 u로 바꾸면 Unsigned가 된다.
+    let _int8_var: i8 = 0; // signed 8-bit integer
+    let _int16_var: i16 = 0; // signed 16-bit integer
+    let _int32_var: i32 = 0; // signed 32-bit integer
+    let _int64_var: i64 = 0; // signed 64-bit integer
+    let _int128_var: i128 = 0; // signed 128-bit integer
+    let _arch_var: isize = 0; // architecture가 32비트이면 32비트 정수, 64비트이면 64비트 정수이다.
 }
