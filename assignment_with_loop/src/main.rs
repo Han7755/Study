02f8d3208d0a0ab103a857a2623cd2c28c603fd1
Han7755 <gksmfgyrud@naver.com>
2022-12-06 13:36:48 +0900

    loop문 작성

--- assignment_with_loop/src/main.rs
+++ assignment_with_loop/src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
     let mut counter = 0; // 수정가능한 변수
+
+    let result = loop{
+        counter += 1;
+        if counter == 10{
+            break counter * 2; // 
+        }
+    }
 }
