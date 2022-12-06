f9deb6c024229ed5eb405fb7ca92b0f9ada390b3
Han7755 <gksmfgyrud@naver.com>
2022-12-06 13:46:33 +0900

    코드 작성

--- for_in_loop/src/main.rs
+++ for_in_loop/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
     let a = [10, 20, 30, 40, 50];
+
+    // for in 문은 파이썬의 for문과 비슷하게 작동
+    for elements in a {
+        println!("{}", elements);
+    }
+    for number in (1..4).rev() {
+        println!("{}", number);
+    }
 }
