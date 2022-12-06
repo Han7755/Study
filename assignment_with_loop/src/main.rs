3c94323f56411b617322690ac9ea7ad731f82bdd
Han7755 <gksmfgyrud@naver.com>
2022-12-06 13:37:47 +0900

    출력문 추가

--- assignment_with_loop/src/main.rs
+++ assignment_with_loop/src/main.rs
@@ -1,10 +1,12 @@
 fn main() {
     let mut counter = 0; // 수정가능한 변수
 
-    let result = loop{
+    let result = loop {
         counter += 1;
-        if counter == 10{
-            break counter * 2; // counter * 2를 반환하며 루프 종료 
+        if counter == 10 {
+            break counter * 2; // counter * 2를 반환하며 루프 종료
         }
-    }
+    };
+
+    println!("The result is {}", result);
 }
