2327350b75b0a03d6bd16792f01e93b926c8b904
Han7755 <gksmfgyrud@naver.com>
2022-12-06 13:16:48 +0900

    주석 추가

--- control_flow/src/main.rs
+++ control_flow/src/main.rs
@@ -12,4 +12,6 @@ fn main() {
     let condition = true;
     let number = if condition { 5 } else { 6 }; // if 문, else문 안의 코드에 세미콜론이 없으므로 if문의 결과에 따라 5 또는 6을 변수에 할당하는 배정문이다.
     println!("Value of number = {}", number);
+
+    // let number = if condition { 5 } else { "six" }; <- 컴파일 타임에 타입이 명확하지 않으므로 오류 발생
 }
